d008d36048379e3366adfe8c968e3171f579bbc3
Oluwatise Ajayi <dr393462@gmail.com>
2025-12-06 11:58:00 +0100

    chore:cleaned up the project by removing the default NestJS boilerplate files that are not needed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { DocumentsModule } from './documents/documents.module';
@@ -11,7 +9,7 @@ import { DocumentsModule } from './documents/documents.module';
     PrismaModule,
     DocumentsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
